331001aa26491481aa57e1f3090353005052cd4d
chvmvd <104971044+chvmvd@users.noreply.github.com>
2024-04-21 21:57:44 +0900

    不要な章を削除

--- docs/1-trial-session/09-loop/_samples/answer-for/script.js
+++
@@ -1,5 +0,0 @@
-let sum = 0;
-for (let i = 1; i <= 10; i += 1) {
-  sum += i;
-}
-document.write(sum);
--- docs/1-trial-session/09-loop/_samples/product/script.js
+++
@@ -1,5 +0,0 @@
-let product = 1;
-for (let i = 1; i <= 10; i = i + 1) {
-  product = product * i;
-}
-document.write(product);
