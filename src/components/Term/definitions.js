7b35038c63bb5f32bb950a08789bc1e752e8d70b
aster <137767097+aster-void@users.noreply.github.com>
2024-04-23 14:09:38 +0900

    fixed broken term links and deleted unused ones

--- src/components/Term/definitions.js
+++ src/components/Term/definitions.js
@@ -1,449 +1,294 @@
 export default {
   referencePageTitles: {
-    "/docs/trial-session/get-started/": "はじめてのWeb開発",
-    "/docs/trial-session/html/": "HTML",
-    "/docs/trial-session/javascript/": "JavaScriptことはじめ",
-    "/docs/trial-session/expressions/": "値と式と演算子",
-    "/docs/trial-session/expressions/#いろいろな演算子": "いろいろな演算子",
-    "/docs/trial-session/variables/": "変数",
-    "/docs/trial-session/boolean/": "論理値と論理演算子",
-    "/docs/trial-session/if-statement/": "条件分岐",
-    "/docs/trial-session/functions/": "関数",
-    "/docs/trial-session/functions/#モジュール化": "モジュール化",
-    "/docs/trial-session/css/": "CSS",
-    "/docs/trial-session/object/": "オブジェクト",
-    "/docs/trial-session/dom/": "DOM",
-    "/docs/trial-session/events/": "イベント",
-    "/docs/trial-session/project/": "プロジェクト",
-    "/docs/trial-session/deploy/": "作ったウェブアプリを公開する",
-    "/docs/browser-apps/inspector/": "ブラウザの開発者ツール",
-    "/docs/browser-apps/reference/": "オブジェクトの参照",
-    "/docs/browser-apps/loop/": "繰り返し",
-    "/docs/browser-apps/class/": "クラス",
-    "/docs/browser-apps/array/": "配列",
-    "/docs/browser-apps/anonymous-function/": "無名関数",
-    "/docs/browser-apps/git-github/": "Git と GitHub",
-    "/docs/web-servers/wsl-setup/": "WSL のセットアップ",
-    "/docs/web-servers/node-js/": "はじめての Node.js",
-    "/docs/web-servers/module/": "モジュールと npm",
-    "/docs/web-servers/server/": "Express によるサーバー構築",
-    "/docs/web-servers/form/": "フォーム",
-    "/docs/web-servers/get-post/": "GET リクエストと POST リクエスト",
-    "/docs/web-servers/database/": "データベース",
-    "/docs/web-servers/cookie/": "Cookie と認証",
-    "/docs/web-servers/deploy-on-render/": "Render へのデプロイ",
-    "/docs/advanced/fetch-api/": "Fetch API",
-    "/docs/advanced/bundler/": "トランスパイラとモジュールバンドラ",
-    "/docs/advanced/typescript/": "TypeScript",
-    "/docs/advanced/react/": "React",
+    "/docs/get-started/": "はじめてのWeb開発",
+    "/docs/html/": "HTML",
+    "/docs/javascript/": "JavaScriptことはじめ",
+    "/docs/expressions/": "値と式と演算子",
+    "/docs/expressions/#いろいろな演算子": "いろいろな演算子",
+    "/docs/variables/": "変数",
+    "/docs/boolean/": "論理値と論理演算子",
+    "/docs/if-statement/": "条件分岐",
+    "/docs/functions/": "関数",
+    "/docs/functions/#モジュール化": "モジュール化",
+    "/docs/css/": "CSS",
+    "/docs/object/": "オブジェクト",
+    "/docs/dom/": "DOM",
+    "/docs/events/": "イベント",
+    "/docs/project/": "プロジェクト",
+    "/docs/deploy/": "作ったウェブアプリを公開する",
   },
   terms: {
     fileExtension: {
       name: "拡張子",
       definition:
         "ファイル名のピリオド以降の部分。ファイルの種類を識別するために用いられる場合がある。Windows では標準では表示されないので、表示する設定にしておくと良い。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     fork: {
       name: "フォーク",
       definition:
         "GitHub において、他人が公開しているリポジトリを複製すること。この操作により、元のリポジトリに対して影響を与えることなくソースコードを変更できる。",
-      referencePage: "/docs/trial-session/get-started/",
+      referencePage: "/docs/get-started/",
     },
     git: {
       name: "Git",
       definition: "分散型バージョン管理システムのひとつ。",
-      referencePage: "/docs/trial-session/get-started/",
+      referencePage: "/docs/get-started/",
     },
     github: {
       name: "GitHub",
       definition:
         "Git を用いて、プログラムの保存、共有、公開などができる、開発者のための総合プラットフォーム。",
-      referencePage: "/docs/trial-session/get-started/",
+      referencePage: "/docs/get-started/",
     },
     gitpod: {
       name: "Gitpod",
       definition:
         "GitHub 等に保存されているプログラムをブラウザ上で編集するためのサービス。",
-      referencePage: "/docs/trial-session/get-started/",
+      referencePage: "/docs/get-started/",
     },
     repository: {
       name: "リポジトリ",
       definition: "ソースコードやそれに付随する情報のまとまり。",
-      referencePage: "/docs/trial-session/get-started/",
+      referencePage: "/docs/get-started/",
     },
     html: {
       name: "HTML",
       definition: "ウェブページの構造を表すためのマークアップ言語。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     htmlNest: {
       name: "ネスト (HTML)",
       definition: "HTML要素の中に他の要素を入れること。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     javascript: {
       name: "JavaScript",
       definition:
         "ウェブサイトに動きを与えるために生まれたスクリプト言語。現在は機能が拡張され、ウェブサイト以外にも応用されるようになった。",
-      referencePage: "/docs/trial-session/javascript/",
+      referencePage: "/docs/javascript/",
     },
     startTag: {
       name: "開始タグ",
       definition:
         "要素の先頭を表す。 <div>内容</div> ならば <div> が開始タグ。属性をつけることができる。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     tag: {
       name: "タグ",
       definition:
         "< から > までで囲まれた領域。<a> を「aタグ」のように読んでいる。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     attribute: {
       name: "属性",
       definition: "HTML要素の特徴を指定する値。開始タグの中に含まれる。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     element: {
       name: "HTML 要素",
       definition: "HTMLの開始タグから終了タグまでの領域。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     endTag: {
       name: "終了タグ",
       definition:
         "要素の終端を表す。 <div>内容</div> ならば </div> が終了タグ。",
-      referencePage: "/docs/trial-session/html/",
+      referencePage: "/docs/html/",
     },
     css: {
       name: "CSS",
       definition:
         "ウェブサイトの見た目をカスタマイズするための言語。 HTML 要素 の style 属性は CSS を用いて記述する。",
-      referencePage: "/docs/trial-session/css/",
+      referencePage: "/docs/css/",
     },
     styleAttribute: {
       name: "style 属性",
       definition:
         "表示可能な多くの HTML 要素に対して設定できる、見た目をカスタマイズするための属性。",
-      referencePage: "/docs/trial-session/css/",
+      referencePage: "/docs/css/",
     },
     cssProperty: {
       name: "プロパティ (CSS)",
       definition:
         "CSS により設定できる見た目の種類。例えば color は文字色、font-size は文字の大きさ。",
-      referencePage: "/docs/trial-session/css/",
+      referencePage: "/docs/css/",
     },
     javascriptStatement: {
       name: "文",
       definition:
         "JavaScriptにおいて、セミコロンによって区切られた命令の単位。ただしセミコロンは省略可能。",
-      referencePage: "/docs/trial-session/javascript/",
+      referencePage: "/docs/javascript/",
     },
     javascriptValue: {
       name: "値 (JavaScript)",
       definition:
         "JavaScript上で扱われる単一のデータ。数値や文字列、論理値、オブジェクトなど。",
-      referencePage: "/docs/trial-session/expressions/",
+      referencePage: "/docs/expressions/",
     },
     javascriptString: {
       name: "文字列 (JavaScript)",
       definition:
         "JavaScriptにおける値の一種。ダブルクォーテーションで囲んでプログラム上に直接記述できる。",
-      referencePage: "/docs/trial-session/expressions/",
+      referencePage: "/docs/expressions/",
     },
     javascriptNumber: {
       name: "数値 (JavaScript)",
       definition: "JavaScriptにおける値の一種。プログラム中に直接記述できる。",
-      referencePage: "/docs/trial-session/expressions/",
+      referencePage: "/docs/expressions/",
     },
     javascriptOperator: {
       name: "演算子 (JavaScript)",
       definition:
         "JavaScript において、主に値に対して作用し、別の値を得るための記号。",
-      referencePage: "/docs/trial-session/expressions/",
+      referencePage: "/docs/expressions/",
     },
     javascriptExpression: {
       name: "式 (JavaScript)",
       definition: "JavaScript において、計算・処理されて値を生成するまとまり。",
-      referencePage: "/docs/trial-session/expressions/",
+      referencePage: "/docs/expressions/",
     },
     javascriptEvaluation: {
       name: "評価 (JavaScript)",
       definition: "JavaScriptの実行エンジンが式から値を生成する動作。",
-      referencePage: "/docs/trial-session/expressions/",
+      referencePage: "/docs/expressions/",
     },
     javascriptOperatorPriority: {
       name: "演算子の優先順位 (JavaScript)",
       definition:
         "複数の演算子を含む式の評価順序を決定するための指標。例えば、乗算演算子は加算演算子より優先順位が高いため、 3 + 4 * 5 は 3 + (4 * 5) と評価される。",
-      referencePage: "/docs/trial-session/expressions/",
+      referencePage: "/docs/expressions/",
     },
     javascriptStringConcatenation: {
       name: "文字列の結合",
       definition:
         "文字列に加算演算子を使うと、一個目の文字列の後ろに二個目の文字列が追加される。",
-      referencePage: "/docs/trial-session/expressions/#いろいろな演算子",
+      referencePage: "/docs/expressions/#いろいろな演算子",
     },
     javascriptVariable: {
       name: "変数",
       definition: "JavaScriptで値を一時的に保存しておくための入れ物。",
-      referencePage: "/docs/trial-session/variables/",
+      referencePage: "/docs/variables/",
     },
     javascriptDeclaration: {
       name: "宣言 (JavaScript)",
       definition: "JavaScriptで変数を使用するために必要な記述。",
-      referencePage: "/docs/trial-session/variables/",
+      referencePage: "/docs/variables/",
     },
     javascriptAssignment: {
       name: "代入 (JavaScript)",
       definition:
         "変数に値を設定する操作。それまで入っていた値は消えてしまう。",
-      referencePage: "/docs/trial-session/variables/",
+      referencePage: "/docs/variables/",
     },
     camelCase: {
       name: "キャメルケース",
       definition:
         "複数の単語にわたるフレーズを、2語目以降の先頭の文字を大文字にして結合する命名規則。例: camelCase",
-      referencePage: "/docs/trial-session/variables/",
+      referencePage: "/docs/variables/",
     },
     snakeCase: {
       name: "スネークケース",
       definition:
         "複数の単語にわたるフレーズを、アンダーバー (_) によって結合する命名規則。例: snake_case",
-      referencePage: "/docs/trial-session/variables/",
+      referencePage: "/docs/variables/",
     },
     pascalCase: {
       name: "パスカルケース",
       definition:
         "複数の単語にわたるフレーズを、それぞれの単語の先頭の文字を大文字にして結合する命名規則。例: PascalCase",
-      referencePage: "/docs/trial-session/variables/",
+      referencePage: "/docs/variables/",
     },
     javascriptBoolean: {
       name: "論理値 (JavaScript)",
       definition:
         "JavaScriptにおける値の一種で、真偽を表すもの。true または false の2種類しかない。",
-      referencePage: "/docs/trial-session/boolean/",
+      referencePage: "/docs/boolean/",
     },
     javascriptIfStatement: {
       name: "if文",
       definition:
         "与えられた真偽値が真である場合のみ{}ブロックの中の処理を実行する制御構造。",
-      referencePage: "/docs/trial-session/if-statement/",
+      referencePage: "/docs/if-statement/",
     },
     javascriptControlFlow: {
       name: "制御構造 (JavaScript)",
       definition:
         "JavaScriptにおいて、通常上から下に実行される文の実行順序を変化させるための言語構造。",
-      referencePage: "/docs/trial-session/if-statement/",
+      referencePage: "/docs/if-statement/",
     },
     javascriptFunction: {
       name: "関数 (JavaScript)",
       definition: "引数や返り値を持つ文のまとまり。",
-      referencePage: "/docs/trial-session/functions/",
+      referencePage: "/docs/functions/",
     },
     javascriptParameter: {
       name: "引数 (JavaScript)",
       definition: "JavaScriptの関数に対し、呼び出し時に与えることのできる値。",
-      referencePage: "/docs/trial-session/functions/",
+      referencePage: "/docs/functions/",
     },
     javascriptPass: {
       name: "～ (値) を ～ (関数) に渡す",
       definition: "JavaScriptの関数に値を引数として設定して実行すること。",
-      referencePage: "/docs/trial-session/functions/",
+      referencePage: "/docs/functions/",
     },
     javascriptReturnValue: {
       name: "戻り値 (JavaScript)",
       definition: "JavaScriptの関数呼び出し式の評価結果となる値。",
-      referencePage: "/docs/trial-session/functions/",
+      referencePage: "/docs/functions/",
     },
     javascriptReturn: {
       name: "～ (値) を返す (JavaScript)",
       definition:
         "関数の実行が終了し、式の評価結果の値が確定すること。return 文により実現できる。",
-      referencePage: "/docs/trial-session/functions/",
+      referencePage: "/docs/functions/",
     },
     javascriptScope: {
       name: "スコープ (JavaScript)",
       definition: "変数が有効な範囲。",
-      referencePage: "/docs/trial-session/functions/",
+      referencePage: "/docs/functions/",
     },
     javascriptModularization: {
       name: "モジュール化",
       definition:
         "ひとまとまりの煩雑なものや操作を一つにまとめて、コードを複数のブロックに分解すること。",
-      referencePage: "/docs/trial-session/functions/#モジュール化",
+      referencePage: "/docs/functions/#モジュール化",
     },
     javascriptObject: {
       name: "オブジェクト (JavaScript)",
       definition:
         "JavaScriptにおける値の一種。内部にプロパティを複数格納できる。プリミティブではない値。",
-      referencePage: "/docs/trial-session/object/",
+      referencePage: "/docs/object/",
     },
     javascriptPrimitive: {
       name: "プリミティブ (JavaScript)",
       definition:
         "JavaScriptにおける値の種類。感覚的には「それ以上分解できない値」",
-      referencePage: "/docs/trial-session/object/",
+      referencePage: "/docs/object/",
     },
     javascriptProperty: {
       name: "プロパティ (JavaScript)",
       definition:
         "JavaScriptのオブジェクトがその内部に持っている値。オブジェクトの属性(情報)。",
-      referencePage: "/docs/trial-session/object/",
+      referencePage: "/docs/object/",
     },
     dom: {
       name: "DOM",
       definition:
         "HTML 構造を JavaScript のオブジェクトとして扱うための枠組み。",
-      referencePage: "/docs/trial-session/dom/",
+      referencePage: "/docs/dom/",
     },
     events: {
       name: "イベント",
       definition:
         "ボタンのクリック、フォームへの入力、ページの読み込みなど、Webページ上で発生するあらゆるアクションの総称。",
-      referencePage: "/docs/trial-session/events/",
+      referencePage: "/docs/events/",
     },
     eventHandler: {
       name: "イベントハンドラ",
       definition:
         "イベント発生時の処理を行う仕組み。onclick 関数、onload 関数など、on キーワードにイベント名をつけた名称になっている。",
-      referencePage: "/docs/trial-session/events/",
-    },
-    javascriptClass: {
-      name: "クラス (JavaScript)",
-      definition:
-        "同じプロパティを持つオブジェクトを統一的に扱うための仕組み。プロパティや関数(メソッド)を予め設定できる。",
-      referencePage: "/docs/browser-apps/class/",
-    },
-    javascriptInstance: {
-      name: "インスタンス (JavaScript)",
-      definition: "new 演算子をクラスに適用して生成されるオブジェクトのこと。",
-      referencePage: "/docs/browser-apps/class/",
-    },
-    javascriptConstructor: {
-      name: "コンストラクタ (JavaScript)",
-      definition:
-        "インスタンスを作成するタイミング（new 演算子をクラスに適用するタイミング）で実行される特殊なメソッド。constructor という名前で定義する必要がある。コンストラクタを定義すると、インスタンスを生成するときにプロパティの設定も同時に行うことができる。",
-      referencePage: "/docs/browser-apps/class/",
-    },
-    javascriptMethod: {
-      name: "メソッド (JavaScript)",
-      definition:
-        "オブジェクトに対して定義された関数。メソッド内ではメソッドが定義されたオブジェクトを this キーワードを用いて参照できる。",
-      referencePage: "/docs/browser-apps/class/",
-    },
-    arrowFunction: {
-      name: "アロー関数",
-      definition:
-        "JavaScript において関数を生成するための式のひとつ。=> という記号が矢のように見えることから命名された。",
-      referencePage: "/docs/browser-apps/anonymous-function/",
-    },
-    callbackFunction: {
-      name: "コールバック関数",
-      definition: "他の関数の引数として呼び出される関数。",
-      referencePage: "/docs/browser-apps/anonymous-function/",
-    },
-    javascriptModule: {
-      name: "モジュール (JavaScript)",
-      definition:
-        "独立したプログラムの塊。JavaScript では、通常 1 つのファイルがモジュールとして扱われる。Node.js では、拡張子が .mjs の場合 import 文を用いてインポートして利用する。",
-      referencePage: "/docs/web-servers/module/",
-    },
-    library: {
-      name: "ライブラリ",
-      definition:
-        "汎用性の高い複数のプログラムを再利用可能な形でひとまとまりにしたもの。基本的に他のプログラムから利用され、単体では実行できない。",
-    },
-    serverClient: {
-      name: "サーバーとクライアント",
-      definition:
-        "サービスを提供する側のコンピューターやソフトウェアをサーバー、提供される側のコンピューターやソフトウェアをクライアントと呼ぶ。例えば Google Chrome などの Web ブラウザは、Web における代表的なクライアントソフトウェアである。",
-      referencePage: "/docs/web-servers/server/",
-    },
-    json: {
-      name: "JSON",
-      definition:
-        "複雑なデータ構造を JavaScript オブジェクトに似た形式で文字列として表現するための記法。",
-      referencePage: "/docs/web-servers/module/",
-    },
-    requestResponse: {
-      name: "リクエストとレスポンス",
-      definition:
-        "クライアントからサーバーに対しサービスを要求する通信をリクエスト、リクエストに対してサーバーからクライアントに応答として返される通信をレスポンスと呼ぶ。",
-      referencePage: "/docs/web-servers/server/",
-    },
-    queryString: {
-      name: "クエリ文字列",
-      definition:
-        "URL の末尾に付与することのできる追加のパラメーター。辞書型のデータを表すことができる。key1=value1&key2=value2 といった構造。日本語文字などはパーセントエンコーディングする必要がある。",
-      referencePage: "/docs/web-servers/form/",
-    },
-    httpHeaderBody: {
-      name: "リクエスト・レスポンスのヘッダ・ボディ (HTTP)",
-      definition:
-        "HTTP においてリクエストやレスポンスは主にヘッダと呼ばれる領域とボディと呼ばれる領域に分かれている。リクエストやレスポンスの本体はボディの部分に格納されており、ヘッダは付加的な情報を与えるために用いられる。なお、GET リクエストではリクエストボディを使用できない。",
-      referencePage: "/docs/web-servers/get-post/",
-    },
-    httpMethod: {
-      name: "HTTP メソッド",
-      definition:
-        "HTTP リクエストの種類。GET や POST などがある。GET リクエストにはリクエストボディが存在しないが、POST リクエストでは利用できる。",
-      referencePage: "/docs/web-servers/get-post/",
-    },
-    npxCommand: {
-      name: "npx コマンド",
-      definition:
-        "npm 付属のコマンド。インストールした npm パッケージを直接実行する機能を持つ。",
-      referencePage: "/docs/web-servers/database/",
-    },
-    asynchronousProcess: {
-      name: "非同期処理",
-      definition:
-        "処理を実行する際、その完了を待たずに呼び出し元の処理が続行されるような処理。JavaScript では、async キーワードをつけた関数は非同期処理を行うための関数となり、内部では await キーワードにより他の非同期処理を呼び出してその結果を待機できるようになる。",
-      referencePage: "/docs/web-servers/database/",
-    },
-    api: {
-      name: "API",
-      definition:
-        "あるコンピュータープログラムの機能や管理するデータなどを、外部の他のプログラムから呼び出して利用するための手順やデータ形式などを定めた規約。アプリケーションプログラミングインターフェース(Application Programming Interface)の略。",
-      referencePage: "/docs/advanced/fetch-api/",
-    },
-    thread: {
-      name: "スレッド",
-      definition:
-        "コンピューターがプログラムの順番どおりに 1 つずつ処理を行う機構。JavaScriptではメインスレッドと呼ばれる単一のスレッドで主に処理が実行される。",
-      referencePage: "/docs/web-servers/database/",
-    },
-    transpile: {
-      name: "トランスパイル",
-      definition:
-        "プログラムを同じ意味を持つ別のプログラムへと変換すること。Web 開発では、通常変換の結果ウェブブラウザが解釈できる JavaScript が生成される。Babel が有名。",
-      referencePage: "/docs/advanced/bundler/",
-    },
-    moduleBundler: {
-      name: "モジュールバンドラ",
-      definition:
-        "複数の JavaScript モジュールをまとめてひとつにするためのソフトウェア。webpack が有名。",
-      referencePage: "/docs/advanced/bundler/",
-    },
-    jsx: {
-      name: "JSX",
-      definition:
-        "JavaScript の拡張構文で、React などと共に用いられる。JSX が有効な JavaScript では、 HTML に似た構文の式により特定の種類のオブジェクトを作成できる。拡張子は .jsx で、TypeScript とともに用いる場合は .tsx。",
-      referencePage: "/docs/advanced/react/",
-    },
-    javascriptTruthyFalsy: {
-      name: "truthy と falsy (JavaScript)",
-      definition:
-        "JavaScript では、数値のゼロ、空文字列、null、undefined、NaN を falsy、それ以外の全ての値を truthy とみなす。if 文は条件式の結果が truthy であるか falsy であるかによって分岐を行う。",
-      referencePage: "/docs/advanced/react/",
-    },
-    mutableImmutable: {
-      name: "ミュータブルとイミュータブル",
-      definition:
-        "オブジェクトの内部が変更可能であるとき、そのオブジェクトはミュータブルであるといい、変更不能である場合はイミュータブルであるという。React などのライブラリでは状態を表すオブジェクトはイミュータブルであることが求められる。これは、状態が変化する際に参照も変化することを保証するためである。",
-      referencePage: "/docs/browser-apps/reference/",
+      referencePage: "/docs/events/",
     },
   },
 };
