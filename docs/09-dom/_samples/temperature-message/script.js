1b6f83d239e691842902e677fbce2048b2cd9a15
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-06-29 01:02:25 +0900

    不等号の順番変更

--- docs/09-dom/_samples/temperature-message/script.js
+++ docs/09-dom/_samples/temperature-message/script.js
@@ -4,7 +4,7 @@ if (temperature < 15) {
   document.getElementById("temperature-message").textContent =
     "今日は寒いです。";
   document.getElementById("temperature-message").style.color = "blue";
-} else if (temperature >= 30) {
+} else if (30 <= temperature) {
   document.getElementById("temperature-message").textContent =
     "今日は暑いです。";
   document.getElementById("temperature-message").style.color = "red";
