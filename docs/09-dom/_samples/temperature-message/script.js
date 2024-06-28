b674075ebff50984c72fcf4a9e56a479c1bd9a4d
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-06-28 23:15:36 +0900

    演習問題の例を修正

--- docs/09-dom/_samples/temperature-message/script.js
+++ docs/09-dom/_samples/temperature-message/script.js
@@ -4,7 +4,7 @@ if (temperature < 15) {
   document.getElementById("temperature-message").textContent =
     "今日は寒いです。";
   document.getElementById("temperature-message").style.color = "blue";
-} else if (temperature > 30) {
+} else if (temperature >= 30) {
   document.getElementById("temperature-message").textContent =
     "今日は暑いです。";
   document.getElementById("temperature-message").style.color = "red";
