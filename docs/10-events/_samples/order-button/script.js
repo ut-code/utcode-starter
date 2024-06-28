0c146541a767bae81b5dcaecda848aacc31c1a59
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-06-28 15:34:06 +0900

    イベントの演習問題の修正箇所対応

--- docs/10-events/_samples/order-button/script.js
+++ docs/10-events/_samples/order-button/script.js
@@ -1,5 +1,5 @@
 function order() {
-  document.getElementById("message").textContent = "注文済です。";
+  document.getElementById("order-message").textContent = "注文済です。";
 }
 
 document.getElementById("order-button").onclick = order;
