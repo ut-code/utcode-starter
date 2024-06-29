2ebaaf04c8d566ef131b760a592c105047239bd7
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-06-29 11:49:25 +0900

    気温の問題の条件分岐の順序を変更

--- docs/09-dom/_samples/temperature-message/script.js
+++ docs/09-dom/_samples/temperature-message/script.js
@@ -4,11 +4,12 @@ if (temperature < 15) {
   document.getElementById("temperature-message").textContent =
     "今日は寒いです。";
   document.getElementById("temperature-message").style.color = "blue";
-} else if (30 <= temperature) {
+} else if (temperature < 30) {
+  document.getElementById("temperature-message").textContent =
+    "今日は快適です。";
+  document.getElementById("temperature-message").style.color = "black";
+} else {
   document.getElementById("temperature-message").textContent =
     "今日は暑いです。";
   document.getElementById("temperature-message").style.color = "red";
-} else {
-  document.getElementById("temperature-message").textContent =
-    "今日は快適です。";
 }
